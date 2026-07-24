6c785db2ec6617e968478d0de730b2b24873032a
Ilkham Abdillah <IlkhamAbdillah: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@github.com>
2026-07-24 21:11:15 +0700

    fix: Player Camera Initialization

--- Assets/Assets/Scripts/PlayerCam.cs
+++ Assets/Assets/Scripts/PlayerCam.cs
@@ -10,6 +10,16 @@ public class PlayerCam : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Vector3 rot = transform.eulerAngles;
+        xRotation = rot.x;
+        yRotation = rot.y;
+    }
+
     private void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
